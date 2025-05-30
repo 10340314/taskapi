e365d8f5cd2900f021a7462970a421be46f302df
10340314 <victor.diaz@sptech.school>
2025-05-30 20:34:37 -0300

    initial project

---
+++ src/main/java/com/study/taskAPI/TaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.taskAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApiApplication.class, args);
+	}
+
+}
